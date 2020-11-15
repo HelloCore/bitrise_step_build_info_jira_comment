d0dbf9c305a415ef883c0255093fcf57bdc6fc21
HelloCore <aukwat@gmail.com>
2020-11-16 00:17:31 +0700

    I don't know

--- get_last_succesfully_hash.sh
+++ get_last_succesfully_hash.sh
@@ -1,4 +1,12 @@
 LAST_BUILD_REGEX='"commit_hash":"([A-z0-9]+)"'
+
+
+echo '----ENV----'
+echo "BITRISE_APP_SLUG: $BITRISE_APP_SLUG"
+echo "BITRISE_GIT_BRANCH: $BITRISE_GIT_BRANCH"
+echo "BITRISE_TRIGGERED_WORKFLOW_TITLE: $BITRISE_TRIGGERED_WORKFLOW_TITLE"
+echo '-----------'
+
 URL="https://api.bitrise.io/v0.1/apps/$BITRISE_APP_SLUG/builds"
 API_RESPONSE=$(curl -s --request GET --url "$URL" \
     --data-urlencode "branch=$BITRISE_GIT_BRANCH" \
@@ -8,6 +16,7 @@ API_RESPONSE=$(curl -s --request GET --url "$URL" \
     -H "Authorization: $access_token")
 
 echo "Calling URL: $URL"
+echo $API_RESPONSE > test.json
 
 if [[ $API_RESPONSE =~ $LAST_BUILD_REGEX ]]; then
     echo "FOUND LAST_COMMIT_SHA ${BASH_REMATCH[1]}"
