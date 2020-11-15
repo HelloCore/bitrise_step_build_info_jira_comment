9c48c70a54ccd490ca5bdf8cead15b86b2d3c27c
HelloCore <aukwat@gmail.com>
2020-11-16 00:28:57 +0700

    Fix step

--- find_issue_logs.sh
+++ find_issue_logs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ROOT_PATH="$(dirname "$0")"
 
 FILTERED_ISSUE_PATH="/tmp/tmp-filtered-issue.txt"
 CURRENT_COMMIT_SHA=$1
@@ -13,11 +14,11 @@ then
     rm $FILTERED_ISSUE_PATH
 fi
 
-ISSUE_LIST=`git log --pretty="%s %b" $CURRENT_COMMIT_SHA..$LAST_SUCCESS_COMMIT_SHA | grep -o "$REGEX_ISSUE"`
+ISSUE_LIST=`git log --pretty="%s %b" $LAST_SUCCESS_COMMIT_SHA..$CURRENT_COMMIT_SHA | grep -o "$REGEX_ISSUE"`
 
 for line in $ISSUE_LIST
 do
-    ./add_if_not_exists.sh "$line" $FILTERED_ISSUE_PATH
+    $ROOT_PATH/add_if_not_exists.sh "$line" $FILTERED_ISSUE_PATH
 done
 
 echo "---- FOUND ISSUES ----"
